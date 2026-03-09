010b572e050056b61b4bbf5752fb28d5cc004e20
KhushiGupta2004 <khushigup2023@gmail.com>
2026-03-09 14:23:02 +0530

    Add files via upload

---
+++ frontend/.next/dev/build/chunks/[turbopack-node]_transforms_postcss_ts_bd888ee4._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/OneDrive/Desktop/txt-speech/frontend/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/49641_531bc76b._.js",
+  "chunks/[root-of-the-server]__e001de0b._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/OneDrive/Desktop/txt-speech/frontend/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
