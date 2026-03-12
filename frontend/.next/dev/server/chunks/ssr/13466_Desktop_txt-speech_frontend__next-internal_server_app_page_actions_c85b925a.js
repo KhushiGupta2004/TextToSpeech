35b0f87da21c5e9d8f7e614bdf1b758193dbccc2
KhushiGupta2004 <khushigup2023@gmail.com>
2026-03-12 21:32:13 +0530

    Add files via upload

---
+++ frontend/.next/dev/server/chunks/ssr/13466_Desktop_txt-speech_frontend__next-internal_server_app_page_actions_c85b925a.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/OneDrive/Desktop/txt-speech/frontend/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=13466_Desktop_txt-speech_frontend__next-internal_server_app_page_actions_c85b925a.js.map
---
+++ frontend/.next/dev/server/chunks/ssr/OneDrive_Desktop_txt-speech_frontend_src_app_42987292._.js
@@ -0,0 +1,22 @@
+module.exports = [
+"[project]/OneDrive/Desktop/txt-speech/frontend/src/app/favicon.ico (static in ecmascript, tag client)", ((__turbopack_context__) => {
+
+__turbopack_context__.v("/_next/static/media/favicon.0b3bf435.ico");}),
+"[project]/OneDrive/Desktop/txt-speech/frontend/src/app/favicon.ico.mjs { IMAGE => \"[project]/OneDrive/Desktop/txt-speech/frontend/src/app/favicon.ico (static in ecmascript, tag client)\" } [app-rsc] (structured image object, ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s([
+    "default",
+    ()=>__TURBOPACK__default__export__
+]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$OneDrive$2f$Desktop$2f$txt$2d$speech$2f$frontend$2f$src$2f$app$2f$favicon$2e$ico__$28$static__in__ecmascript$2c$__tag__client$29$__ = __turbopack_context__.i("[project]/OneDrive/Desktop/txt-speech/frontend/src/app/favicon.ico (static in ecmascript, tag client)");
+;
+const __TURBOPACK__default__export__ = {
+    src: __TURBOPACK__imported__module__$5b$project$5d2f$OneDrive$2f$Desktop$2f$txt$2d$speech$2f$frontend$2f$src$2f$app$2f$favicon$2e$ico__$28$static__in__ecmascript$2c$__tag__client$29$__["default"],
+    width: 256,
+    height: 256
+};
+}),
+];
+
+//# sourceMappingURL=OneDrive_Desktop_txt-speech_frontend_src_app_42987292._.js.map
